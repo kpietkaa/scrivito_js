c381a4992e59e6ee3ff341528f7fd8204ae7a643
Kazik Pietka <kazimierz.pietka@infopark.de>
2017-08-21 11:27:57 +0200

    Add title for blogOverview

--- src/widgets/blog_overview_widget.js.jsx
+++ src/widgets/blog_overview_widget.js.jsx
@@ -3,6 +3,7 @@ import BlogPostPreviewList from 'components/blog_post_preview_list';
 const BlogOverviewWidget = Scrivito.createWidgetClass({
   name: 'BlogOverviewWidget',
   attributes: {
+    title: 'string',
     maxItems: 'integer',
     tag: 'string',
   },
@@ -10,8 +11,13 @@ const BlogOverviewWidget = Scrivito.createWidgetClass({
 
 Scrivito.provideUiConfig(BlogOverviewWidget, {
   title: 'Blog Overview',
+  titleForContent: widget => widget.get('title'),
   description: 'A widget with up to four blog posts',
   attributes: {
+    title: {
+      title: 'Title',
+      description: 'Title section title',
+    },
     maxItems: {
       title: 'Max Items',
       description: 'Amount of items to show. Leave it empty, to show all items',
@@ -24,9 +30,15 @@ Scrivito.provideUiConfig(BlogOverviewWidget, {
 });
 
 Scrivito.provideComponent(BlogOverviewWidget, widget =>
-  <BlogPostPreviewList
-    maxItems={ widget.get('maxItems') }
-    tag={ widget.get('tag') } />
+  <div>
+    <Scrivito.React.Content
+      tag='h3'
+      content={ widget }
+      attribute='title' />
+    <BlogPostPreviewList
+      maxItems={ widget.get('maxItems') }
+      tag={ widget.get('tag') } />
+  </div>
 );
 
 export default BlogOverviewWidget;
